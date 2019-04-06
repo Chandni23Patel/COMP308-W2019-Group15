62f0053b676dc4269de8c02079885a1dd3404a56
chandni <chandptl.91@gmail.com>
2019-04-06 19:22:29 -0400

    mlab database is added

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
-"URI" : "mongodb://localhost/test",
+//"URI" : "mongodb://localhost/test",
+"URI":"mongodb://chandni:a123456@ds053251.mlab.com:53251/comp308-w2019-healthcare",
 "secret": "SomeSecret"
 }
